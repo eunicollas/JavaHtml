1f027bdc4fc7d31ea61e98439c12fa9bd0d2d0d9
eunicollas <80913209+eunicollas@users.noreply.github.com>
2021-06-18 10:49:27 -0300

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,25 @@
+let header = document.querySelector('header');
+let h1 = document.createElement('h1');
+header.appendChild(h1);
+h1.innerHTML = "Integração Html + Java Script";
+
+let main = document.querySelector('main');
+let div1 = document.createElement('div');
+main.appendChild(div1);
+
+let primeiroh2 = document.createElement('h2');
+div1.appendChild(primeiroh2);
+primeiroh2.innerHTML = "Red Dead Redemption 2";
+let primeiroP = document.createElement('p');
+div1.appendChild(primeiroP);
+primeiroP.innerHTML = "Red Dead Redemption 2 é um jogo eletrônico de ação-aventura desenvolvido e publicado pela Rockstar Games. É o terceiro título da série Red Dead e uma prequela de Red Dead Redemption, tendo sido lançado em outubro de 2018 para PlayStation 4 e Xbox One e em novembro de 2019 para Microsoft Windows e Google Stadia.";
+
+let div2 = document.createElement('div');
+main.appendChild(div2);
+
+let segundoh2 = document.createElement('h2')
+div2.appendChild(segundoh2);
+segundoh2.innerHTML = "The Last of Us Part II"
+let segundoP = document.createElement('p');
+div2.appendChild(segundoP);
+segundoP.innerHTML = "The Last of Us Part II é um jogo eletrônico de ação-aventura desenvolvido pela Naughty Dog e publicado pela Sony Interactive Entertainment. É o segundo jogo da franquia e foi lançado em 19 de junho de 2020 exclusivamente para PlayStation 4.."
